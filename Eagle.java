c912462f59fa7f2477cdc83794557b4a2f60d991
0_Nocaudie <o.nocaudie@googlemail.com>
2019-10-01 12:26:02 +0200

    Eye Cherry believes he can fly !

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,44 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    //implémentation de l'interface
+
+    @Override
+    public void takeOff() {
+        if (!this.flying && this.altitude == 0);
+            this.flying = true;
+            System.out.println(this.getName() + " deploys its wings and take off !");
+    }
+
+    @Override
+    public int ascend(int meters) {
+        if (isFlying()) {
+            this.altitude+= meters;
+            System.out.println(this.getName() + " ascended to the height of " + this.altitude);
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public int descend(int meters) {
+        if (isFlying()) {
+            this.altitude += -meters;
+            System.out.println(this.getName() + " descended to the height of " + this.altitude);
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public void land() {
+        if (isFlying() && this.altitude == 1) {
+            System.out.println(this.getName() + " disastrously landed on the ground to pee...");
+        } else {
+            System.out.println("Control Tower, how high am I ?");
+            System.out.println(this.getName() + ", You are still at altitude " + this.altitude + ", keep descending...");
+        }
+
+    }
+
+
 }
--- Nature.java
+++ Nature.java
@@ -13,15 +13,16 @@ public class Nature {
 
         Eagle eyeCherry = new Eagle("Eye Cherry");
         // TODO : décommente le code suivant afin de le tester
-        /*
+
         eyeCherry.takeOff();
         eyeCherry.ascend(120);
         eyeCherry.ascend(30);
         eyeCherry.glide();
         eyeCherry.descend(140);
         eyeCherry.land();
-        eyeCherry.descend(9);
+        eyeCherry.descend(8);
+        eyeCherry.descend(1);
         eyeCherry.land();
-        */
+
     }
 }
